f31e19729a4d063f06fd17dee0e6f43ed68b832d
Emma-Nangida <emmanangida@gmail.com>
2023-02-08 19:56:49 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,48 @@
+fun main() {
+    greetings("Jane")
+    greetings("Wina")
+    modulus (50,20)
+    addition(50,80)
+    addition(30,10)
+    factaboutme()
+
+
+
+
+
+}
+
+
+fun greetings(name: String){
+    println("hello"+name)
+}
+
+
+
+    fun modulus(num1: Int,num2: Int) {
+        var modulus = num1 % num2
+        println(modulus)
+    }
+
+fun addition(num1 : Int,num2:Int){
+    var sum =num1 + num2
+    println(sum)
+
+    }
+
+fun factaboutme(){
+
+    println("beautiful heart")
+}
+
+
+
+
+
+
+
+
+
+
+
+
